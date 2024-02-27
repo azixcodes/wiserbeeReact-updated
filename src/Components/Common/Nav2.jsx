f6bf19f331379564238c3190e89e1abd72cd123f
azezcodes <aziz@swatitech.com>
2024-02-27 16:45:49 +0500

    mini sidebar,toggle

--- src/Components/Common/Nav2.jsx
+++ src/Components/Common/Nav2.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useRef, useEffect } from "react";
-import { ChevronDown, Search, ChevronUp } from "lucide-react";
+import { ChevronDown, Search, ChevronUp, MenuIcon } from "lucide-react";
 import { notificationSvg } from "../../Constant/svgs";
 import { countries } from "../../Constant/languages";
 import { userSvg } from "../../Constant/svgs";
 import Flag from "react-world-flags";
 
-const Nav2 = () => {
+const Nav2 = (props) => {
+  const { handleToggleSidebar } = props;
   const dropDownRef = useRef(null);
 
   const [openDropDown, setOpenDropdown] = useState(false);
@@ -36,70 +37,67 @@ const Nav2 = () => {
   return (
     <>
       <div className="navbarWrapper d-flex justify-content-between align-items-center flex-wrap">
-        <h4 className="mb-0">Dashboard</h4>
+        <h4 className="mb-0">
+          <MenuIcon onClick={handleToggleSidebar} />
+          Dashboard
+        </h4>
         <div className="searchBox d-flex align-items-center ">
           <Search />
           <input type="text" placeholder="Search" className="w-100" />
         </div>
-          <div className="position-relative ">
-            <span>{notificationSvg}</span>
-            <div className="chip position-absolute d-flex justify-content-center align-items-center text-white">
-              5
-            </div>
+        <div className="position-relative ">
+          <span>{notificationSvg}</span>
+          <div className="chip position-absolute d-flex justify-content-center align-items-center text-white">
+            5
           </div>
-          <div className="d-flex align-items-center position-relative ">
-            <Flag
-              code={language.code}
-              fallback={<span>Unknown</span>}
-              height="16"
-            />
+        </div>
+        <div className="d-flex align-items-center position-relative ">
+          <Flag
+            code={language.code}
+            fallback={<span>Unknown</span>}
+            height="16"
+          />
+          {openDropDown ? (
+            <div className="flagsDropdown custom-scrollbar ">
+              {countries.map((flag, index) => (
+                <div
+                  key={index}
+                  className="d-flex align-items-center  2 flagList"
+                  onClick={() => handleListClick(flag.code, flag.language)}
+                >
+                  <Flag
+                    code={flag.code}
+                    fallback={<span>Unknown</span>}
+                    height="16"
+                  />
+                  <span>{flag.language}</span>
+                </div>
+              ))}
+            </div>
+          ) : null}
+          <div className="d-flex align-items-center" ref={dropDownRef}>
+            <span className="m-0 p-0 d-none d-md-block mx-2">
+              {language.language}
+            </span>
             {openDropDown ? (
-              <div className="flagsDropdown custom-scrollbar ">
-                {countries.map((flag, index) => (
-                  <div
-                    key={index}
-                    className="d-flex align-items-center  2 flagList"
-                    onClick={() =>
-                      handleListClick(flag.code, flag.language)
-                    }
-                  >
-                    <Flag
-                      code={flag.code}
-                      fallback={<span>Unknown</span>}
-                      height="16"
-                    />
-                    <span>{flag.language}</span>
-                  </div>
-                ))}
-              </div>
-            ) : null}
-            <div className="d-flex align-items-center" ref={dropDownRef}>
-              <span className="m-0 p-0 d-none d-md-block mx-2">
-                {language.language}
-              </span>
-              {openDropDown ? (
-                <ChevronUp
-                  className="cursor-pointer"
-                />
-              ) : (
-                <ChevronDown
-                  className="cursor-pointer"
-                />
-              )}
-            </div>
+              <ChevronUp className="cursor-pointer" />
+            ) : (
+              <ChevronDown className="cursor-pointer" />
+            )}
           </div>
-          <div className="d-flex justify-content-between align-items-center userAccount">
-            <div className="navAvatar d-flex justify-content-center align-items-center">
-              {userSvg}
-            </div>
-            <div className="align-items-center userName mx-2">
-              <h6 className="fs-6  fw-bold ">john doe</h6>
-              <h5 className="font-sm ">Admin</h5>
-            </div>
-            <div className="userUpIcon">
-              <ChevronDown className="text-end" />
-            </div>
+        </div>
+        <div className="d-flex justify-content-between align-items-center userAccount">
+          <div className="navAvatar d-flex justify-content-center align-items-center">
+            {userSvg}
           </div>
+          <div className="align-items-center userName mx-2">
+            <h6 className="fs-6  fw-bold ">john doe</h6>
+            <h5 className="font-sm ">Admin</h5>
+          </div>
+          <div className="userUpIcon">
+            <ChevronDown className="text-end" />
+          </div>
+        </div>
       </div>
     </>
   );
