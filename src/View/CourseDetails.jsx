1ab3000627df0e20bae55c14c5022b12834196fd
Kashif <muhammadkashif@swatitech.com>
2024-03-01 15:54:43 +0500

    update code

--- src/View/CourseDetails.jsx
+++ src/View/CourseDetails.jsx
@@ -3,7 +3,7 @@ import { Dot, PauseIcon, UserRound } from "lucide-react";
 import { ChevronDown } from "lucide-react";
 import { ChevronUp } from "lucide-react";
 import { logoFrame as viserbe } from "../Constant/images";
-import { studenticon } from "../Constant/svgs";
+import { greenPlayBtn,purplePlayBtn,lightPlayBtn,multiUser,reviewUser } from "../Constant/svgs";
 import Avatar from "../Components/Common/Avatar";
 import { userSvg } from "../Constant/svgs";
 import Video from "../assets/videos/node.mp4";
@@ -26,7 +26,7 @@ const CourseDetails = () => {
             </div>
             <div className="reviewAboutChannel postion-relative top-4">
               <h1 className="pt-3 fw-bold">Mobile Dev React Native</h1>
-              <div className="bottom-part d-flex flex-wrap ">
+              <div className="bottom-part d-flex justify-content-between flex-wrap ">
                 <div className=" d-flex flex-wrap gap-2  w-50">
                   <div className="img-viserbe ">
                     <img src={viserbe} className="img-fluid" alt="" />
@@ -38,18 +38,18 @@ const CourseDetails = () => {
                 </div>
                 <div className="icons border-bottom-5 d-flex gap-5 pt-2">
                   <div>
-                    <PauseIcon />
+                    {multiUser}
                     &nbsp;
                     <span>2.1k</span>
                   </div>
                   <div>
-                    <PauseIcon />
+                    {reviewUser}
                     &nbsp;
                     <span>2.1k</span>
                   </div>
                 </div>
               </div>
-              <hr className="line w-75" />
+              <hr className="line w-100" />
               <div className=" container-fluid">
                 <div className="row">
                   <div>
@@ -72,9 +72,9 @@ const CourseDetails = () => {
                   </div>
                 </div>
               </div>
-              <hr className="line w-75" />
+              <hr className="line w-100" />
               {/* Review section  */}
-              <h3 className="fw-bold p-0 m-0">Review</h3>
+              <h3 className="fw-bold p-0 m-0 mb-3">Review</h3>
               <div className="container-fluid">
                 <div className="d-flex gap-4">
                   <Avatar>{userSvg}</Avatar>
@@ -88,58 +88,11 @@ const CourseDetails = () => {
                   </div>
                 </div>
               </div>
-              <div className="container-fluid">
-                <div className="d-flex gap-4">
-                  <Avatar>{userSvg}</Avatar>
-                  <div>
-                    <span className="viser-helper">Titania S</span>
-                    <p className="last-text">Today</p>
-                    <p className="first-text">
-                      I loved the course, it had been a long time since I had
-                      experimented with watercolors and now I will do <br /> it
-                      more often thanks.
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="container-fluid">
-                <div className="d-flex gap-4">
-                  <Avatar>{userSvg}</Avatar>
-                  <div>
-                    <span className="viser-helper">Zhirkov</span>
-                    <p className="last-text">Today</p>
-                    <p className="first-text">
-                      Yes. I just emphasize that the use of Photoshop, for
-                      non-users, becomes difficult to follow. What requires{" "}
-                      <br /> a course to master it. Safe and very didactic
-                      teacher.
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="container-fluid">
-                <div className="d-flex gap-4">
-                  <Avatar>{userSvg}</Avatar>
-                  <div>
-                    <span className="viser-helper">Miphoska</span>
-                    <p className="last-text">Today</p>
-                    <p className="first-text">
-                      I haven't finished the course yet, as I would like to have
-                      some feedback from the teacher, about the <br /> comments
-                      I shared on the forum 3 months ago, and I still haven't
-                      had any answer. <br />I think the course is well
-                      structured, however the explanations and videos are very
-                      quick for beginners. <br /> However, it is good to go
-                      practicing.
-                    </p>
-                  </div>
-                </div>
-              </div>
             </div>
           </div>
 
           {/* Second container */}
-          <div className=" second col-md-12 col-lg-4   rounded-5 shadow-sm">
+          <div className=" second col-md-12 col-lg-4 rounded-5 pb-5 shadow-sm align-self-start">
             <div className="container">
               <div className="upper pt-4">
                 <div className="d-flex justify-content-between">
@@ -179,11 +132,11 @@ const CourseDetails = () => {
                   <span className=" colored-heading fs-6 ">
                     Installing Vue JS
                   </span>
-                  <span className=" completed p-1 fs-6 fw-lighter d-flex justify-content-center text-white rounded-1">
+                  <span className="cursor-pointer completed p-1 fs-6 fw-lighter d-flex justify-content-center text-white rounded-1">
                     Completed
                   </span>
                 </div>
-                <PauseIcon />
+                {greenPlayBtn}
                 <span className="colored-time-2">10m</span>
               </div>
               <div className="upper  pt-3 pb-2 border-bottom">
@@ -191,46 +144,46 @@ const CourseDetails = () => {
                   <span className=" colored-heading fs-6">
                     Understand Vue Component
                   </span>
-                  <span className=" playing mt-2  p-2   d-flex justify-content-center text-white rounded-1">
+                  <span className="cursor-pointer playing mt-2  p-2   d-flex justify-content-center text-white rounded-1">
                     Playing
                   </span>
                 </div>
-                <PauseIcon />
+                {purplePlayBtn}
                 <span className="colored-time">59m</span>
               </div>
               <div className="upper  pt-3 pb-2 border-bottom">
                 <div className="d-flex justify-content-between ">
                   <span className=" colored-heading fs-6 ">Vue Templating</span>
                 </div>
-                <PauseIcon className="icon" />
+                {lightPlayBtn}
                 <span className="icon-time">12m</span>
               </div>
               <div className="upper  pt-3 pb-2 border-bottom">
                 <div className="d-flex justify-content-between ">
                   <span className=" colored-heading fs-6 ">Vue Forms</span>
                 </div>
-                <PauseIcon className="icon" />
+                {lightPlayBtn}
                 <span className="icon-time">Document</span>
               </div>
               <div className="upper  pt-3 pb-2 border-bottom">
                 <div className="d-flex justify-content-between ">
                   <span className=" colored-heading fs-6 ">Vue Styling</span>
                 </div>
-                <PauseIcon className="icon" />
+                {lightPlayBtn}
                 <span className="icon-time">57m</span>
               </div>
               <div className="upper  pt-3 pb-2 border-bottom">
                 <div className="d-flex justify-content-between ">
                   <span className=" colored-heading fs-6 ">Vue Routing</span>
                 </div>
-                <PauseIcon className="icon" />
+                {lightPlayBtn}
                 <span className="icon-time">1h 30m</span>
               </div>
               <div className="upper  pt-3 pb-2">
                 <div className="d-flex justify-content-between ">
                   <span className=" colored-heading fs-6 ">Vue Animation</span>
                 </div>
-                <PauseIcon className="icon" />
+                {lightPlayBtn}
                 <span className="icon-time">1h 19m</span>
               </div>
             </div>
