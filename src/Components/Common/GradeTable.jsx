70b8a797b4bdad5fb4c490b09903cd1db3981ee8
Kashif <muhammadkashif@swatitech.com>
2024-03-07 17:00:09 +0500

    update table code

--- src/Components/Common/GradeTable.jsx
+++ src/Components/Common/GradeTable.jsx
@@ -45,66 +45,137 @@ const GradeTable = ({ headings, data }) => {
   ];
 
   return (
-    <div className="table-responsive gradesTable">
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            {headings.map((head, index) => (
-              <th key={index}>{head}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((d, index) => (
-            <tr key={index} className="align-middle">
-              <td>{d.name}</td>
-              <td>{d.classLevel}</td>
-              <td>{d.attendance}</td>
-              <td>{d.firstTerm}</td>
-              <td>{d.midTerm}</td>
-              <td>{d.overallScore}</td>
-              <td>
-                <div className="dropdown">
-                  <div
-                    className="iconWrapper"
-                    type="button"
-                    onClick={() => toggleDropdown(index)}
-                    id={`dropdownMenu-${index}`}
-                  >
-                    <MoreVertical />
-                  </div>
-                  <ul
-                    className={`dropdown-menu gradeTableDropdown p-0 py-1 ${
-                      openIndexes.includes(index) ? "show" : ""
-                    }`}
-                  >
-                    {items.map((item, i) => (
-                      <li key={i} className="px-2 rounded">
-                        <a
-                          className="dropdown-item  optionLists align-items-center rounded  mt-1 d-flex justify-content-between"
-                          href="#"
+    // <div className="table-responsive gradesTable">
+    //   <table className="table table-striped">
+    //     <thead>
+    //       <tr>
+    //         {headings.map((head, index) => (
+    //           <th key={index}>{head}</th>
+    //         ))}
+    //       </tr>
+    //     </thead>
+    //     <tbody>
+    //       {data.map((d, index) => (
+    //         <tr key={index} className="align-middle">
+    //           <td>{d.name}</td>
+    //           <td>{d.classLevel}</td>
+    //           <td>{d.attendance}</td>
+    //           <td>{d.firstTerm}</td>
+    //           <td>{d.midTerm}</td>
+    //           <td>{d.overallScore}</td>
+    //           <td>
+    //             <div className="dropdown">
+    //               <div
+    //                 className="iconWrapper"
+    //                 type="button"
+    //                 onClick={() => toggleDropdown(index)}
+    //                 id={`dropdownMenu-${index}`}
+    //               >
+    //                 <MoreVertical />
+    //               </div>
+    //               <ul
+    //                 className={`dropdown-menu gradeTableDropdown p-0 py-1 ${
+    //                   openIndexes.includes(index) ? "show" : ""
+    //                 }`}
+    //               >
+    //                 {items.map((item, i) => (
+    //                   <li key={i} className="px-2 rounded">
+    //                     <a
+    //                       className="dropdown-item  optionLists align-items-center rounded  mt-1 d-flex justify-content-between"
+    //                       href="#"
+    //                     >
+    //                       <span
+    //                         className="h6 p-0 m-0"
+    //                         style={
+    //                           i === 1
+    //                             ? { color: "#BF2929" }
+    //                             : { color: "black" }
+    //                         }
+    //                       >
+    //                         {item.label}
+    //                       </span>{" "}
+    //                       {item.icon}
+    //                     </a>
+    //                   </li>
+    //                 ))}
+    //               </ul>
+    //             </div>
+    //           </td>
+    //         </tr>
+    //       ))}
+    //     </tbody>
+    //   </table>
+    // </div>
+    <div class="container-table100">
+      <div class="wrap-table100">
+        <div class="table100 ver5 m-b-110">
+          <div class="table100-head">
+            <table>
+              <thead>
+                <tr class="row100 head">
+                  {headings.map((head, index) => (
+                    <th className={`cell100 column${index+1}`} key={index}>{head}</th>
+                  ))}
+                </tr>
+              </thead>
+            </table>
+          </div>
+
+          <div class="table100-body">
+            <table>
+              <tbody>
+                {data.map((item, index) => (
+                  <tr className="row100 body " key={index}>
+                    <td className={`cell100 column1`}>{item.name}</td>
+                    <td className={`cell100 column2`}>{item.classLevel}</td>
+                    <td className={`cell100 column3`}>{item.attendance}</td>
+                    <td className={`cell100 column4`}>{item.firstTerm}</td>
+                    <td className={`cell100 column5`}>{item.midTerm}</td>
+                    <td className={`cell100 column6`}>{item.overallScore}</td>
+                    <td className={`cell100 column7`}>
+                      <div className="dropdown d-flex justify-content-center">
+                        <div
+                          className="iconWrapper "
+                          type="button"
+                          onClick={() => toggleDropdown(index)}
+                          id={`dropdownMenu-${index}`}
                         >
-                          <span
-                            className="h6 p-0 m-0"
-                            style={
-                              i === 1
-                                ? { color: "#BF2929" }
-                                : { color: "black" }
-                            }
-                          >
-                            {item.label}
-                          </span>{" "}
-                          {item.icon}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+                          <MoreVertical />
+                        </div>
+                        <ul
+                          className={`dropdown-menu gradeTableDropdown p-0 py-1 ${openIndexes.includes(index) ? "show" : ""
+                            }`}
+                        >
+                          {items.map((item, i) => (
+                            <li key={i} className="px-2 rounded">
+                              <a
+                                className="dropdown-item  optionLists align-items-center rounded  mt-1 d-flex justify-content-between"
+                                href="#"
+                              >
+                                <span
+                                  className="h6 p-0 m-0"
+                                  style={
+                                    i === 1
+                                      ? { color: "#BF2929" }
+                                      : { color: "black" }
+                                  }
+                                >
+                                  {item.label}
+                                </span>{" "}
+                                {item.icon}
+                              </a>
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Constant/grades.js
+++ src/Constant/grades.js
@@ -31,6 +31,38 @@ export const data = [
     midTerm: "90/100",
     overallScore: "85/100",
   },
+  {
+    name: "eneh mercy",
+    classLevel: "Primary",
+    attendance: "25/30",
+    firstTerm: "85/100", // Corrected spelling
+    midTerm: "90/100",
+    overallScore: "85/100",
+  },
+  {
+    name: "john doe",
+    classLevel: "secondary",
+    attendance: "15/30",
+    firstTerm: "65/100",
+    midTerm: "10/100",
+    overallScore: "85/100",
+  },
+  {
+    name: "eneh mercy",
+    classLevel: "Higher Secondary",
+    attendance: "25/30",
+    firstTerm: "85/100",
+    midTerm: "90/100",
+    overallScore: "85/100",
+  },
+  {
+    name: "eneh mercy",
+    classLevel: "secondary",
+    attendance: "25/30",
+    firstTerm: "85/100",
+    midTerm: "90/100",
+    overallScore: "85/100",
+  },
 ];
 
 export const headings = [
