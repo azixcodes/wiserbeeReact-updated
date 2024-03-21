4443a5b20ea01a00fcaa350400ab76e570b4ea6c
azezcodes <aziz@swatitech.com>
2024-03-21 16:04:03 +0500

    report card design

---
+++ src/Constant/observation.js
@@ -0,0 +1,30 @@
+export const observation = [
+  {
+    title: "Attentiveness",
+    options: ["Excellent", "Meeting Expectations", "Need Improvement", "Poor"],
+  },
+  {
+    title: "Follow instructions",
+    options: ["Excellent", "Meeting Expectations", "Need Improvement", "Poor"],
+  },
+  {
+    title: "Positive contribution to class",
+    options: ["Excellent", "Meeting Expectations", "Need Improvement", "Poor"],
+  },
+  {
+    title: "Organized work",
+    options: ["Excellent", "Meeting Expectations", "Need Improvement", "Poor"],
+  },
+  {
+    title: "Works carefully and neat",
+    options: ["Excellent", "Meeting Expectations", "Need Improvement", "Poor"],
+  },
+  {
+    title: "Complete tasks",
+    options: ["Excellent", "Meeting Expectations", "Need Improvement", "Poor"],
+  },
+  {
+    title: "Work willingly and without frustration",
+    options: ["Excellent", "Meeting Expectations", "Need Improvement", "Poor"],
+  },
+];
--- src/modals/ReportCardsModel.jsx
+++ src/modals/ReportCardsModel.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import FlexBtn from "../Components/Common/FlexBtn";
 import { exportSvg } from "../Constant/svgs";
+import { observation } from "../Constant/observation";
 
 const ReportCardsModel = ({ onRequestClose }) => {
-
   const student = {
-    'Student ID': '10001',
-    'Student Name': 'John Smith',
-    'Class': 'Primary',
-    'Course ID': 'CS101',
-    'Overall Results': '92%',
-    'Overall Grade': 'A+'
+    "Student ID": "10001",
+    "Student Name": "John Smith",
+    Class: "Primary",
+    "Course ID": "CS101",
+    "Overall Results": "92%",
+    "Overall Grade": "A+",
   };
   const subjectsData = [
     {
@@ -93,6 +93,83 @@ const ReportCardsModel = ({ onRequestClose }) => {
               ))}
             </tbody>
           </table>
+        </div>
+        <div className="Table_Card">
+          <table
+            style={{
+              width: "100%",
+              borderCollapse: "collapse",
+              fontFamily: "arial, sans-serif",
+            }}
+          >
+            <thead>
+              <tr>
+                <th className="Salary_Heading">Assesment</th>
+                <th className="Salary_Heading">Neurocognitive</th>
+                <th className="Salary_Heading">Scholastic</th>
+                <th className="Salary_Heading">Behavioral</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="Salary_Data">Scores</td>
+                <td className="Salary_Data">75%</td>
+                <td className="Salary_Data">75%</td>
+                <td className="Salary_Data">85%</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div className="container-fluid m-0 p-0 mt-3">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="teacherObservation w-100">
+                <div className="observationHeader px-4">
+                  <h6 className=" text-start">Teacher Observation</h6>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12">
+              <div className="observationWrapper w-100 d-flex flex-column gap-3 py-2 px-2 mt-4">
+                {observation.map((ob, index) => (
+                  <div
+                    className="observation w-100 px-4 d-flex  justify-content-between align-items-center"
+                    key={index}
+                  >
+                    <label className="fw-bold">{ob.title}</label>
+                    <div className="observationOptions d-flex flex-column gap-2">
+                      <div className="options d-flex gap-2">
+                        {ob.options.map((op, i) => (
+                          <div className="form-check" key={i}>
+                            <input
+                              className="form-check-input"
+                              type="radio"
+                              name="flexRadioDefault"
+                              id="flexRadioDefault1"
+                            />
+                            <label
+                              className="form-check-label"
+                              for="flexRadioDefault1"
+                            >
+                              {op}
+                            </label>
+                          </div>
+                        ))}
+                      </div>
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Write your comments here"
+                      />
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
         </div>
         {/* <div className="Report_button">
           <button onClick={onRequestClose}>Cancel</button>
